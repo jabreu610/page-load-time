7f6512e01c31ee64337bc98fafb3dbb6cab5181c
Jordan Abreu <jabreu610@gmail.com>
2023-09-15 03:35:47 -0400

    adds persistent aggregate stats

--- src/background.js
+++ src/background.js
@@ -1,8 +1,16 @@
 // Setting a toolbar badge text
 browser.runtime.onMessage.addListener((request, sender) => {
   // This cache stores page load time for each tab, so they don't interfere
-  browser.storage.local.get('cache').then(data => {
+  browser.storage.local.get(['cache', 'stat']).then(data => {
     if (!data.cache) data.cache = {};
+    if (!data.stat) data.stat = {};
+    let duration = Math.round(request.timing.duration);
+    if (data.stat[sender.url]) {
+      let {sum, count, min, max, ...rest} = data.stat[sender.url];
+      data.stat[sender.url] = {sum: sum + duration, count: count + 1, min: Math.min(min, duration), max: Math.max(max, duration), ...rest};
+    } else {
+      data.stat[sender.url] = {sum: duration, count: 1, min: duration, max: duration, since: Date.now()};
+    }
     data.cache['tab' + sender.tab.id] = request.timing;
     browser.storage.local.set(data).then(() => {
       browser.browserAction.setBadgeText({text: request.time, tabId: sender.tab.id});
--- src/popup.js
+++ src/popup.js
@@ -13,10 +13,33 @@ function set(id, start, end, noacc) {
 
 browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
   var tab = tabs[0];
-  browser.storage.local.get('cache').then(data => {
+  browser.storage.local.get(['cache', 'stat']).then(data => {
     var t = data.cache['tab' + tab.id];
     total = t.duration;
 
+    if (data.stat[tab.url]) {
+      let {sum, count, min, max, since} = data.stat[tab.url];
+      let avg = Math.round(sum / count);
+      document.getElementById('stat-since').innerHTML = `since ${new Date(since).toLocaleString()}`;
+      document.getElementById('stat-count').innerHTML = count;
+      document.getElementById('stat-total').innerHTML = sum;
+      document.getElementById('stat-avg').innerHTML = avg;
+      document.getElementById('stat-low').innerHTML = min;
+      document.getElementById('stat-high').innerHTML = max;
+    }
+
+    document.getElementById('stat-reset').addEventListener('click', () => {
+      delete data.stat[tab.url];
+      browser.storage.local.set(data).then(() => {
+        document.getElementById('stat-since').innerHTML = '';
+        document.getElementById('stat-count').innerHTML = '';
+        document.getElementById('stat-total').innerHTML = '';
+        document.getElementById('stat-avg').innerHTML = '';
+        document.getElementById('stat-low').innerHTML = '';
+        document.getElementById('stat-high').innerHTML = '';
+      });
+    });
+    
     // https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/NavigationTiming/Overview.html#processing-model
     set('redirect', t.redirectStart, t.redirectEnd);
     set('dns', t.domainLookupStart, t.domainLookupEnd);
